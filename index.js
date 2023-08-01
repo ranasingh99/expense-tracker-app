637ec1932e067338d7ec0609304e4f7b8780949e
ranasingh99 <sanketverma852@gmail.com>
2023-08-01 16:53:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// set the total amount of money kept reserverd for expenditure
+let totalAmount = document.getElementById('total-amount');
+let tempAmount = 0;
+
+totalAmountButton.addEventListener("click", () => {
+  tempAmount = totalAmount.value;
+  //empty or negative input
+  if (tempAmount === "" || tempAmount < 0) {
+    errorMessage.classList.remove("hide");
+  } else {
+    errorMessage.classList.add("hide");
+    //Set Budget
+    amount.innerHTML = tempAmount;
+    //Set Balance
+    balanceValue.innerText = tempAmount - expenditureValue.innerText;
+    //Clear Input Box
+    totalAmount.value = "";
+  }
+});
